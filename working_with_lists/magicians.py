09680cfc18600dee1bf463067a99357b9060bcfb
Nishan <109782567+nishan979@users.noreply.github.com>
2023-08-07 21:33:15 +0600

    Create magicians.py

---
+++ working_with_lists/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'devid', 'carolina']
+for magician in magicians:
+    print(magician)
+    print(f"{magician.title()}, that was a great thick!")
+    print(f"I can't wait to see your next trick, {magician.title()}")
